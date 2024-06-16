ccd6ef1cc49aa19b0c063a4249384445aa31efa2
leixin <yongxin@iscas.ac.cn>
2024-06-16 14:31:27 +0800

    add base network

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,95 @@
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// TCPPeer represents the remote node over a TCP established connection
+type TCPPeer struct {
+	// The underlying connection of the peer. Which in this case
+	// is a TCP connection
+	net.Conn
+
+	// if we dial and retrieve a conn => outbound == True
+	// if we accept and retrieve a conn => outbound == false
+	outbound bool 
+
+	wg *sync.WaitGroup
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		Conn: conn,
+		outbound: outbound,
+		wg: &sync.WaitGroup{},
+	}
+
+}
+
+func (p *TCPPeer) CloseStream() {
+	p.wg.Done()
+}
+
+func (p *TCPPeer) Send(b []byte) error {
+	_, err := p.Conn.Write(b)
+	return err
+}
+
+type TCPTransportOpts struct {
+	ListenAddr string
+	HandshakeFunc HandshakeFunc
+	Decoder       Decoder
+	OnPeer        func(Peer) error
+}
+
+type TCPTransport struct {
+	TCPTransportOpts
+	listener net.Listener
+	rpcch    chan RPC
+}
+
+func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
+	return &TCPTransport{
+		TCPTransportOpts: opts,
+		rpcch:            make(chan RPC, 1024),
+	}
+}
+
+
+func (t *TCPTransport) handleConn(conn net.Conn, outbound bool) {
+	var err error
+
+	defer func() {
+		fmt.Printf("dropping peer connection: %s", err)
+		conn.Close()
+	}()
+
+	peer := NewTCPPeer(conn, outbound)
+
+	if err = t.HandshakeFunc(peer); err != nil {return}
+
+	if t.OnPeer != nil {
+		if err = t.OnPeer(peer); err != nil {return}
+	}
+
+	for {
+		rpc := RPC{}
+		err = t.Decoder.Decode(conn, &rpc)
+		if err != nil {return}
+
+		rpc.From = conn.RemoteAddr().String()
+
+		if rpc.Stream {
+			peer.wg.Add(1)
+			fmt.Printf("[%s] incoming stream, waiting...\n", conn.RemoteAddr())
+			peer.wg.Wait()
+			fmt.Printf("[%s] stream closed, resuming read loop\n", conn.RemoteAddr())
+			continue
+		}
+
+		t.rpcch <- rpc
+	}
+
+}
